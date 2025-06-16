733482cc09fe6b4b5178c6b6f961b518904d6e66
KnickQueue <155283151+KnickQueue@users.noreply.github.com>
2025-06-16 13:35:22 -0400

    Ensure Node types recognized

--- components/icons/HelpCircleIcon.tsx
+++ components/icons/HelpCircleIcon.tsx
@@ -6,14 +6,14 @@ interface IconProps {
 }
 
 export const HelpCircleIcon: React.FC<IconProps> = ({ className }) => (
-  <svg 
-    xmlns="http://www.w3.org/2000/svg" 
-    viewBox="0 0 24 24" 
-    fill="none" 
-    stroke="currentColor" 
-    strokeWidth="2" 
-    strokeLinecap="round" 
-    strokeLinejoin="round" 
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     className={className}
   >
     <circle cx="12" cy="12" r="10"></circle>
@@ -21,4 +21,4 @@ export const HelpCircleIcon: React.FC<IconProps> = ({ className }) => (
     <line x1="12" y1="17" x2="12.01" y2="17"></line>
   </svg>
 );
-    
+
